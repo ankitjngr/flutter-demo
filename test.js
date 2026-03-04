de6e86d1980e3c42116019b0813504a98f9d35c7
ankit <anktjngr94@gmail.com>
2026-03-04 23:49:40 +0530

    Refactor code to implement closures and encapsulation with click limiter functionality

--- test.js
+++ test.js
@@ -1,5 +1,60 @@
-console.log('Hello, World!');
-const add = (a, b) => a + b; 
-console.log(add(2, 3)); // Output: 5
-const subtract = (a, b) => a - b;
-console.log(subtract(5, 2)); // Output: 3
+// Closures
+
+function abc() {
+    let a = 10;
+    return function () {
+        console.log(a);
+    }
+}
+
+let myFunc = abc();
+
+// myFunc(); 
+
+
+// Encapsulation
+
+// function createCounter() {
+//     let count = 0;
+//     return {
+//         increment: function () {
+//             count++;
+//             console.log(count);
+//         },
+//         decrement: function () {
+//             count--;
+//             console.log(count);
+//         },
+//         getCount: function () {
+//             return count;
+//         }
+//     };     
+// }
+
+// let counter = createCounter();
+// counter.increment(); // 1
+// counter.increment(); // 2
+// counter.decrement(); // 1
+// console.log(counter.getCount()); // 1
+
+function clickLimiter () {
+    let clickCount = 0;
+    return function () {
+        if (clickCount < 5) {
+            clickCount++;
+            console.log(`Button clicked ${clickCount} times`);
+        } else {
+            console.log('Click limit reached');
+        }
+    }
+}
+
+let limitedClick = clickLimiter();
+
+// Simulating button clicks
+limitedClick(); // Button clicked 1 times
+limitedClick(); // Button clicked 2 times
+limitedClick(); // Button clicked 3 times
+limitedClick(); // Button clicked 4 times
+limitedClick(); // Button clicked 5 times
+limitedClick(); // Click limit reached
